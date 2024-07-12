b68f422e679fde763f5a94f4b1d3830adcaf4f9b
Bruno Santos <bruno.santos@fsdynamics.pt>
2024-07-12 13:33:37 +0100

    redistributePar: Recovered correct Make/options settings and removed our header changes from the main C file, which no longer has our changes.

--- applications/utilities/parallelProcessing/redistributePar/redistributePar.C
+++ applications/utilities/parallelProcessing/redistributePar/redistributePar.C
@@ -5,10 +5,8 @@
     \\  /    A nd           | Copyright (C) 2011-2023 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
- 2014-02-21 blueCAPE Lda: Modifications for blueCFD-Core 2.3
-------------------------------------------------------------------------------
 License
-    This file is a derivative work of OpenFOAM.
+    This file is part of OpenFOAM.
 
     OpenFOAM is free software: you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by
@@ -22,16 +20,6 @@ License
 
     You should have received a copy of the GNU General Public License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
-
-Modifications
-    This file has been modified by blueCAPE's unofficial mingw patches for
-    OpenFOAM.
-    For more information about these patches, visit:
-        http://bluecfd.com/Core
-
-    Modifications made:
-      - Derived from the patches for blueCFD 2.1 and 2.2.
-      - Changed how the tolerance is handled in 32bit Windows.
 
 Application
     redistributePar
