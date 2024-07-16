f05e2acb60ba5ca0bb6abdbe29cfa812cf5820d3
Bruno Santos <bruno.santos@fsdynamics.pt>
2024-07-16 16:04:12 +0100

    src/ThermophysicalTransportModels/fluid/laminar/unityLewisFourier/unityLewisFourier.C: Implemented hack that allows us to go past 'species/linear.H'.

--- src/ThermophysicalTransportModels/fluid/laminar/unityLewisFourier/unityLewisFourier.C
+++ src/ThermophysicalTransportModels/fluid/laminar/unityLewisFourier/unityLewisFourier.C
@@ -24,8 +24,18 @@ License
 \*---------------------------------------------------------------------------*/
 
 #include "unityLewisFourier.H"
+
+#if defined(WIN32) || defined(WIN64)
+#define FINITE_VOLUME_LOAD
+#endif
+
 #include "fvmLaplacian.H"
 #include "fvcSnGrad.H"
+
+#if defined(WIN32) || defined(WIN64)
+#undef FINITE_VOLUME_LOAD
+#endif
+
 #include "surfaceInterpolate.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
