387fa505e6814e565ae9e9a368eccbc04c83fbeb
Bruno Santos <bruno.santos@fsdynamics.pt>
2024-07-17 15:53:00 +0100

    Undid the various force loads that were hacked into C files, because we now have a cleaner way of doing this for most cases.

--- src/radiationModels/radiationModels/fvDOM/radiativeIntensityRay/radiativeIntensityRay.C
+++ src/radiationModels/radiationModels/fvDOM/radiativeIntensityRay/radiativeIntensityRay.C
@@ -24,17 +24,7 @@ License
 \*---------------------------------------------------------------------------*/
 
 #include "radiativeIntensityRay.H"
-
-#if defined(WIN32) || defined(WIN64)
-#define FINITE_VOLUME_LOAD
-#endif
-
 #include "fvm.H"
-
-#if defined(WIN32) || defined(WIN64)
-#undef FINITE_VOLUME_LOAD
-#endif
-
 #include "fvDOM.H"
 #include "constants.H"
 
